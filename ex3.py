8bb054a1e1f0c58fe44b3baf5aa24fc168fc19dd
qiwsir <qiwsir@gmail.com>
2014-01-28 21:59:13 +0800

    code ex3.py

---
+++ ex3.py
@@ -0,0 +1,30 @@
+#coding:utf-8
+#每一种编程语言都包含处理数字和进行数学计算的方法。
+#程序员...如果他们真是数学天才，他们早就去从事数学相关行业了，而不是写写广告程序和社交网络游戏，从人们身上偷赚点小钱而已。
+
+print "I will now count my chickens:"
+
+print "Hesn",25+30/6
+print "Roosters",100-25*3%4    #先计算25*3，结果75；再计算75%4，即75除以4得到的余数为3。如果是75/4，结果为75除以4得到的整数商，为18.然后计算100-3,得97
+print "The floating point number of Roosters",100-25.0*3%4.0   #the result is 97.0
+    
+print "Now I will count the eggs:"
+
+print "3+2+1-5+4%2-1/4+6 is:",3+2+1-5+4%2-1/4+6
+
+print "3+2+1-5+4%2.0-1/4.0+6 the result of floating point number is:",3+2+1-5+4%2.0-1/4.0+6    #the result is 6.75 ,1/4.0=0.25
+
+print "Is it true that 3+2<5-7?"
+
+print 3+2<5-7
+
+print "What is 3+2?",3+2
+print "What is 5-7?",5-7
+
+print "Oh,that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?",5>-2
+print "Is it greater or equal?",5>=-2
+print "Is it less or equal?",5<=-2
