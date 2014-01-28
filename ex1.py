319894fba7e7d22dfe58307627a2698796ca9f79
qiwsir <qiwsir@gmail.com>
2014-01-28 21:18:17 +0800

    code ex1.py

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#-- coding:utf-8 --   #不管是否有ASCII编码错误，输入这个都是非常保险的。这是实际编程中的保险方法。 
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
