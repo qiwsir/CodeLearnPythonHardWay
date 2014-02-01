0951e642c7d389d9592a8e352688fc3ebe30815a
qiwsir <qiwsir@gmail.com>
2014-02-01 10:08:26 +0800

    commit ex9 and ex10.py

---
+++ ex10.py
@@ -0,0 +1,16 @@
+#--coding:utf-8--
+tabby_cat = "\tI'm tabbed in."  #\t  缩进tab，一边是8个字符
+persian_cat = "I'm split\non a line."    #\n 换行
+backslash_cat = "I'm \\ a \\ cat."    #\转义，\\意思是要打印符号\
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
---
+++ ex9.py
@@ -0,0 +1,13 @@
+#--coding:utf-8--
+days = "Mon Tue Wed Thu Fri Sta Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nAug"    #/n 表示换行，/为转义符
+
+print "Here are the days:",days
+print "Here are the months:",months
+
+print """
+There's somethin going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, ro 5, or 6.
+"""
